5f78987ecacf89d3d82ff270e5eaf5ad1c78a2f8
stayuyi <stayuyi@163.com>
2018-09-24 00:59:34 +0800

    tayu@DESKTOP-4EF0O2J MINGW64 /d/IdeaProjects/workspace/studySSH (master) $ git add . warning: LF will be replaced by CRLF in .idea/workspace.xml. The file will have its original line endings in your working directory.
    stayu@DESKTOP-4EF0O2J MINGW64 /d/IdeaProjects/workspace/studySSH
    (master)
    $ git commit TestGit.java
    error: pathspec 'TestGit.java' did not match any file(s) known to git.
    
    stayu@DESKTOP-4EF0O2J MINGW64 /d/IdeaProjects/workspace/studySSH
    (master)
    $ git commit
    This is first add file
    with '#' will be ignored, and an empty message aborts the commit.

---
+++ src/main/java/cn/base/TestGit.java
@@ -0,0 +1,15 @@
+package cn.base;
+
+/**
+ * Created by yuyi on 2018/9/24.
+ */
+
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("this is first new Class");
+    }
+
+
+
+
+}
