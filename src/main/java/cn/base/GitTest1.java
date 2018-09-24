6133e8aee7c76c8179e47853aaddb564dffc36e5
stayuyi <stayuyi@163.com>
2018-09-24 10:31:31 +0800

    can not commit red files

---
+++ src/main/java/cn/base/GitTest1.java
@@ -0,0 +1,11 @@
+package cn.base;
+
+/**
+ * Created by yuyi on 2018/9/24.
+ */
+
+public class GitTest1 {
+    public static void main(String[] args) {
+        System.out.println("this is second Class");
+    }
+}
