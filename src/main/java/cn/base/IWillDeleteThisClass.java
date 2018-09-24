907e24eadb13332e620fb334344b7639a211383c
stayuyi <stayuyi@163.com>
2018-09-24 18:53:28 +0800

    I will delete this Class

---
+++ src/main/java/cn/base/IWillDeleteThisClass.java
@@ -0,0 +1,14 @@
+package cn.base;
+
+/**
+ * Created by yuyi on 2018/9/24.
+ */
+
+public class IWillDeleteThisClass {
+    public static void main(String[] args) {
+        System.out.println("这个是测试删除命令的");
+    }
+
+
+
+}
